df91f4ff694c2c50b224d28b8c2a09c124562557
ChenME <ibelieve1210@163.com>
2020-06-08 19:06:28 +0800

    tmp

--- app/src/main/java/mm/chenme/lib/commutillibdemo/ui/other/CoroutineActivity.kt
+++ app/src/main/java/mm/chenme/lib/commutillibdemo/ui/other/CoroutineActivity.kt
@@ -28,6 +28,7 @@ class CoroutineActivity(override val layoutResId: Int = R.layout.act_coroutine) 
     override fun initListener() {
         tv_start.onClick {
             tv_output.text = ""
+            tv_start.isEnabled = false
 
             val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob) //初始化 CoroutineScope 指定协程的运行所在线程传入 Job 方便后面取消协程
             uiScope.launch { //启动一个协程
@@ -37,6 +38,7 @@ class CoroutineActivity(override val layoutResId: Int = R.layout.act_coroutine) 
             object : CountDownTimer(10000L, 500) {
                 override fun onFinish() {
                     updateText("倒计时结束")
+                    tv_start.isEnabled = true
                 }
 
                 override fun onTick(millisUntilFinished: Long) {
@@ -55,10 +57,11 @@ class CoroutineActivity(override val layoutResId: Int = R.layout.act_coroutine) 
 
     private suspend fun updateUI() {
         delay(10000L) //delay是一个 suspend 函数
+        repeat(300) {} // 执行空操作，稍微做一个延时，确保下面的文字可以打印出来
         updateText("协程结束！")
     }
 
-    private fun updateText(text:String){
+    private fun updateText(text: String) {
         tv_output.text = "$text\n${tv_output.text}"
     }
 }
